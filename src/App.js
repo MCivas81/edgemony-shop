3d822444f371648c5020ea30008e2da9fa85d143
Marco Civello <marco.civello@gmail.com>
2021-03-07 20:49:10 +0100

    fixed searchbar - added hidden overflow to the body when modal is open

--- src/App.js
+++ src/App.js
@@ -7,16 +7,12 @@ import { useState, useEffect } from "react";
 
 import "./App.css";
 
-// const fakeProducts = require("./mocks/data/products.json");
-
 const data = {
   title: "Edgemony Shop",
   description: "A fake e-commerce with a lot of potential",
-  logo:
-    "https://edgemony.com/wp-content/uploads/2020/03/cropped-Logo-edgemony_TeBIANCO-04.png",
+  logo: "https://edgemony.com/wp-content/uploads/2020/03/cropped-Logo-edgemony_TeBIANCO-04.png",
   cover:
     "https://images.pexels.com/photos/4123897/pexels-photo-4123897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-  // products: fakeProducts,
 };
 
 function App() {
@@ -25,11 +21,6 @@ function App() {
   const [isError, setError] = useState(false);
   const [retry, setRetry] = useState(false);
   const [input, setInput] = useState("");
-
-  function search(e) {
-    const target = e.target.value;
-    setInput(target);
-  }
 
   useEffect(() => {
     setLoading(true);
@@ -55,16 +46,12 @@ function App() {
         alt="Company_img"
       />
       <input
-        onChange={search}
-        id="search"
+        onChange={(e) => setInput(e.target.value)}
+        id="searchBar"
         type="text"
         placeholder="Search..."
       />
-      {!isLoading ? (
-        <ListItems products={products} input={input} />
-      ) : (
-        <Loading />
-      )}
+      {!isLoading ? <ListItems products={products} input={input} /> : <Loading />}
       {isError && <Error retry={retry} setRetry={setRetry} />}
     </div>
   );
--- src/components/ModalProduct.js
+++ src/components/ModalProduct.js
@@ -3,24 +3,26 @@ import React from "react";
 import "./ModalProduct.css";
 
 function ModalProduct({ isOpen, closeModal, image, title, price, description }) {
-  return isOpen ? (
-    <div className="modal">
-      <div className="modal-content">
-        <img src={image} alt="product_img" />
-        <div className="modal-text">
-          <h2>{title}</h2>
-          <p>{description}</p>
-          <hr />
-          <span className="modal-price">Price: €{price}</span>
+  return isOpen
+    ? (document.body.style.overflow = "hidden") && (
+        <div className="modal">
+          <div className="modal-content">
+            <img src={image} alt="product_img" />
+            <div className="modal-text">
+              <h2>{title}</h2>
+              <p>{description}</p>
+              <hr />
+              <span className="modal-price">Price: €{price}</span>
+            </div>
+            <div>
+              <button onClick={() => closeModal()} type="button" className="modal-btn">
+                X
+              </button>
+            </div>
+          </div>
         </div>
-        <div>
-          <button onClick={() => closeModal()} type="button" className="modal-btn">
-            X
-          </button>
-        </div>
-      </div>
-    </div>
-  ) : null;
+      )
+    : (document.body.style.overflow = "unset") && null;
 }
 
 export default ModalProduct;
