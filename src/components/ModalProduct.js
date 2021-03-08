8e281f9d5de74562616af2750099ca6307511bc8
Marco Civello <marco.civello@gmail.com>
2021-03-08 22:05:29 +0100

    added total products and total prices in header

--- src/components/ModalProduct.js
+++ src/components/ModalProduct.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ModalProduct.css";
 
-function ModalProduct({ isOpen, closeModal, image, title, price, description }) {
+function ModalProduct({ isOpen, closeModal, product, setCart }) {
+  const [isInCart, setIsInCart] = useState(false);
+
+  function addToCart() {
+    setCart((prevState) => [...prevState, { ...product }]);
+  }
+
   return isOpen
     ? (document.body.style.overflow = "hidden") && (
         <div className="modal">
           <div className="modal-content">
-            <img src={image} alt="product_img" />
+            <img src={product.image} alt="product_img" />
             <div className="modal-text">
-              <h2>{title}</h2>
-              <p>{description}</p>
+              <h2>{product.title}</h2>
+              <p>{product.description}</p>
               <hr />
-              <span className="modal-price">Price: €{price}</span>
+              <div className="modal-bottomContent">
+                <button
+                  disabled={isInCart}
+                  onClick={() => {
+                    addToCart();
+                    setIsInCart(true);
+                  }}
+                  className="modal-cartBtn"
+                  type="button">
+                  Add to Cart
+                </button>
+                <span className="modal-price">Price: €{product.price}</span>
+              </div>
             </div>
             <div>
               <button onClick={() => closeModal()} type="button" className="modal-btn">
@@ -22,7 +40,7 @@ function ModalProduct({ isOpen, closeModal, image, title, price, description }) 
           </div>
         </div>
       )
-    : (document.body.style.overflow = "unset") && null;
+    : (document.body.style.overflow = "") && null;
 }
 
 export default ModalProduct;
